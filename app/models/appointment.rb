d4e2d01a7b55a290e80a5a1ad6227937d4b1adfe
jasonchilcott <jasonchilcottcode@gmail.com>
2020-09-23 19:06:28 -0400

    added all the stuff in the tests not in the readme

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
 
   def display_dt
-    self.appointment_datetime.strftime('%B %e, %Y at %l:%M')
+    self.appointment_datetime.strftime('%B %e, %Y at %k:%M')
   end
 end
